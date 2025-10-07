82604b4cbe48373ceec4cde3cc37564e17159e97
Abdul Junaid <55615812+abduljunaid02@users.noreply.github.com>
2025-10-08 01:14:24 +0200

    Add files via upload

---
+++ binary_search_tree.py
@@ -0,0 +1,29 @@
+#In a binary search tree, the nodes to the left must be smaller than
+#parent node and to the right bigger than root node.
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+# Self is the name of the object we create later on
+
+root = Node(13)
+node7 = Node(7)
+node15 = Node(15)
+node3 = Node(3)
+node8 = Node(8)
+node14 = Node(14)
+node19 = Node(19)
+node18 = Node(18)
+
+root.left = node7
+root.right = node15
+
+node7.left = node3
+node7.right = node8
+
+node15.left = node14
+node15.right = node19
+
+node19.left = node18
